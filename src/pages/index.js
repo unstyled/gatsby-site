060ba5cee83bd2c8072d3b38893da7a1a25ea4d2
Si Jobling <simon.jobling@gmail.com>
2020-04-15 04:15:57 +0100

    Change introduction text

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,9 +8,10 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>Unstyled is a multimedia agency creating audio, visual and physical productions.</h1>
+
+    <p>We make podcasts, websites, books, events and much more.</p>
+    <p>Challenge us! We’re a creative group of people who like to help people make inspiring products that engage with a range of audiences, using our skills as relatable leaders in tech and education.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
